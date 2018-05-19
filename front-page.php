c6e73e1480bf8d4d7557557dbc1c7117f19eb1e4
Ralph N <ralph@n.com>
2018-05-19 10:06:05 -0700

    creating front page template

--- front-page.php
+++ front-page.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * The template for displaying front page
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -14,6 +9,9 @@
 
 get_header();
 ?>
+	<div class="hero-area">
+		<div class="cover">asdasdas</div>
+	</div>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
@@ -36,5 +34,4 @@ get_header();
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
